9aa6cc4f1a256ff061865fcd583fa9cbad474654
Carlos Riascos <carlosriascos@Carlos-Riascoss-MacBook.local>
2012-02-04 14:08:21 -0500

    Added show loading to KBTableView

--- Koombea/View/KBTableView.m
+++ Koombea/View/KBTableView.m
@@ -13,6 +13,7 @@
 
 @synthesize backgroundView;
 @synthesize tableData;
+@synthesize loadingIndicator = _loadingIndicator;
 
 - (void)showAlert:(NSString *)title message:(NSString *)message cancelTitle:(NSString *)cancelTitle okTitle:(NSString *)okTitle
 {
@@ -29,7 +30,17 @@
 {
     [self showAlert:@"Error" message:message cancelTitle:@"OK" okTitle:nil];
 }
+- (void)showLoading:(UIViewController *)viewCtrl withText:(NSString *)text
+{
+    [self.navigationController.view addSubview:_loadingIndicator];
+    _loadingIndicator.labelText = text;
+    [_loadingIndicator show:YES];
+}
 
+- (void)hideLoading:(UIViewController *)viewCtrl
+{
+    [_loadingIndicator removeFromSuperview];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
